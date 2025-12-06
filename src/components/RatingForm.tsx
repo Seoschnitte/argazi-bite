fa93c3da2c468ae6ed020c69afa308e12837b600
Seoschnitte <84650626+Seoschnitte@users.noreply.github.com>
2025-12-06 14:49:57 +0500

    Updated RatingForm.tsx

--- src/components/RatingForm.tsx
+++ src/components/RatingForm.tsx
@@ -121,7 +121,7 @@ export function RatingForm({ fishTypes, telegramUserId, onClose, onSuccess }: Ra
         onClose();
       }, 2000);
     } catch (err) {
-      setError(err instanceof Error ? err.message : 'Ошибка! Включите геолокацию на устройстве.');
+      setError(err instanceof Error ? err.message : 'Включите геолокацию на устройстве.');
     } finally {
       setLoading(false);
       setCheckingLocation(false);
